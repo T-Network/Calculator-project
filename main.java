dedc4a4c1c6565b1cf3640f3dca71cf459c4dd92
Temiloluwa Olasunkanmi <rd028182@student.reading.ac.uk>
2026-08-15 23:54:31 +0100

    first commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        ArrayList<Integer> UserInput = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        
+    }
+}
