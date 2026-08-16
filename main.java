d1a3a7b797e8e0f304accd62a50d9f91ef49605f
Temiloluwa Olasunkanmi <rd028182@student.reading.ac.uk>
2026-08-16 19:05:02 +0100

    Added comments

--- main.java
+++ main.java
@@ -3,18 +3,21 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        ArrayList<Integer> UserInput = new ArrayList<Integer>();
+        //ArrayList<Integer> UserInput = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
         int end = 1;
         double n = 3.1415;
+        //Allows the program to loop continously 
         while (end != 0) {
             System.out.print("Enter a number: ");
+            
             if (n == 3.1415) {
                 float a = scanner.nextFloat();
                 float b = scanner.nextFloat();
                 System.out.println("You entered: " + a + " and " + b);
                 // Scanner operators = new Scanner(System.in);
                 String calc = scanner.next();
+                //Math operators
                 switch (calc) {
                     case "w":
                         n = (a + b);
@@ -43,6 +46,7 @@ public class main {
                 System.out.println("You entered: " + n + " and " + b);
                 // Scanner operators = new Scanner(System.in);
                 String calc = scanner.next();
+                //The ans feature so a previous result can be used in further calculations
                 switch (calc) {
                     case "w":
                         n = (n + b);
@@ -64,6 +68,7 @@ public class main {
                         System.out.println("Error");
 
                 }
+                //Allows user to exit program
                 System.out.println("do you want to exit program?");
                 end = scanner.nextInt();
                 System.out.println("Program ended");
