0c889be2910fa180e256e59fc7ad7e534e4bc61d
Temiloluwa Olasunkanmi <rd028182@student.reading.ac.uk>
2026-08-16 01:03:29 +0100

    Integer calculations work and user input works

--- main.java
+++ main.java
@@ -6,6 +6,30 @@ public class main {
         ArrayList<Integer> UserInput = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("You entered: " + a + " and " + b);
+        //Scanner operators = new Scanner(System.in);
+        String calc = scanner.next();
+        switch (calc) {
+            case "w": 
+                System.out.println(a + b);
+            break;
+            case "x":
+                System.out.println(a - b);
+            break;
+            case "y":
+                System.out.println(a * b);
+            break;
+            case "z":
+                System.out.println(a / b);
+            break;
+            default:
+                System.out.println("Error");
         
+
+        }
+
+
     }
 }
